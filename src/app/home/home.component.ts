4359b0c8d3d49b3c5aa4c3c273e50542af0ed4bf
Cristian-Marius Buda <buda.cristian@protonmail.com>
2023-11-19 09:19:58 +0200

    Final Project Front-end

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,45 @@
+import {Component} from '@angular/core';
+import {environment} from "../../environments/environment";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  title: string = environment.appName;
+  logo: string = environment.appLogo;
+  owner: string = environment.appOwner;
+
+  items: Array<any> = [
+    {
+      title: 'Home',
+      route: '/home',
+    },
+    {
+      title: 'Admin',
+      route: '/dashboard',
+    },
+    {
+      title: 'Logout',
+      route: '/auth',
+    },
+  ];
+
+ // imageSalesLeft: string = 'assets/sales_left.jpg';
+  imageSalesMiddle: string = 'assets/sales_middle.png';
+  //imageSalesRight: string = 'assets/sales_right.jpg';
+
+  constructor(private router: Router) {
+  }
+
+  onChangePage(page: any) {
+    console.log(page);
+    this.router.navigateByUrl(page.route);
+  }
+
+  onLogout(): void {
+    this.router.navigateByUrl('/auth')
+  }
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+// generated with ng g environments
+
+export const environment = {
+  apiUrl:"http://localhost:8080",
+  appName: "Auto Shop",
+  appOwner: "Buda Cristian Marius",
+  appLogo: "https://i.pinimg.com/736x/ff/48/a2/ff48a211592c1708e6fdbd1f67d6f255.jpg"
+};
